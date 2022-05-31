7d32f0e4fc10b96a8527441eaccd59c28d306ff4
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-05-31 18:19:46 -0300

    crud django tentando fazer

---
+++ crud/kiko_projeto/Kiko/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+# declare a new model with a name "GeeksModel"
+class KikoModel(models.Model):
+  
+    # fields of the model
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+  
+    # renames the instances of the model
+    # with their title name
+    def __str__(self):
+        return self.title
